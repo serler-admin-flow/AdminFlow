7fadd27140b5b14018878c3238b776f680bf1b03
uditchoudhary <uditchoudhary@gmail.com>
2015-09-25 10:47:30 +0000

    cucumber setup

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
  root 'welcome#homepage'
+ 
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
