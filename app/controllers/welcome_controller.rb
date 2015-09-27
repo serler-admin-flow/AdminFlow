216a1d17e4382f2c30efce249eef1ef1881f1983
heluva <callumjohns@yahoo.co.nz>
2015-09-27 07:45:54 +0000

    adjusted files

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,14 @@
 class WelcomeController < ApplicationController
   def homepage
+    
   end
+  
+  def user
+    
+  end 
   
   def biblio
     @biblio = biblio.html.erb
-  
-    
   end
   
 end
