73c14d973516fc450436b47a9dd9edc6a554d3e3
heluva <callumjohns@yahoo.co.nz>
2015-09-28 00:06:29 +0000

    user functionality completed. am currently running unit tests on the features

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   # before_filter :authenticate_user!
 
   before_action :authenticate_user!
-  before_action :admin_only, :except => :show
+  # before_action :admin_only, :except => :show
 
   def index
     @users = User.all
