f8e2396f75a4fe4170f9b6f83dbebb1c32dd3250
vigneshd7 <vignesh.d7@zoho.com>
2015-10-19 10:29:40 +0000

    mailer trial 1

---
+++ app/mailers/mailer.rb
@@ -0,0 +1,11 @@
+class Mailer < ActionMailer::Base
+    default :from => "serlergamma@gmail.com"
+    default template_path: 'devise/mailer'  
+ 
+
+ def reset_password_instructions(user,authenticity_token,commit)
+    @user = user
+    @token = authenticity_token
+    mail(to: @user.email, subject: "SERLER Account activation")
+ end
+end
