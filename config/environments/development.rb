c3501f67397be25cf0b588b484da451290a621b6
vigneshd7 <vignesh.d7@zoho.com>
2015-10-20 10:12:02 +0000

    mailer trail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,7 +43,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default :charset => "utf-8"
-  config.action_mailer.default_url_options = { :host => 'https://serler-adminworkflow-vigneshd7-2.c9.io' }
+  config.action_mailer.default_url_options = { :host => 'https://serler-admin-flow.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     enable_starttls_auto: true,
