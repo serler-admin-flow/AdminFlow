6fac1c0a9524b5ce49c626b2be4085b05472b75e
vigneshd7 <vignesh.d7@zoho.com>
2015-10-21 01:31:03 +0000

    Mailer testing

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -5,7 +5,6 @@ ActionMailer::Base.smtp_settings = {
     :authentication => :plain,
     :user_name => 'serlergamma@gmail.com',
     :password => 'teamgamma',
-    :domain => 'smtp.gmail.com',
+    :domain => 'heroku.com',
     :enable_starttls_auto => true,
-    :tls => true
 }
