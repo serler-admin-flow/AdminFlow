df745c62647c4a5ea9cccfd8118517338aa59872
vigneshd7 <vignesh.d7@zoho.com>
2015-10-21 04:27:50 +0000

    changes to create account

--- config/environment.rb
+++ config/environment.rb
@@ -3,15 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-# Configuration for using SendGrid on Heroku
-ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
-  :port           => '587',
-  :authentication => :plain,
-  :user_name      => 'v.boss7@gmail.com',
-  :password       => 'VI.GOOGLE',
-  :domain         => '[lets get this party started!.com]',
-  :enable_starttls_auto => true
-}
-ActionMailer::Base.delivery_method = :smtp
--- spec/support/helpers/session_helpers.rb
+++ spec/support/helpers/session_helpers.rb
@@ -1,10 +1,10 @@
 module Features
   module SessionHelpers
-    def sign_up_with(email, password, confirmation)
+    def sign_up_with(email, password, password_confirmation)
       visit new_user_registration_path
       fill_in 'Email', with: email
       fill_in 'Password', with: password
-      fill_in 'Confirmation', :with => confirmation
+      fill_in 'Password confirmation', :with => password_confirmation
       click_button 'Sign up'
     end
 
